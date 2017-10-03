768dd060382acb9b686c9af55f405378835260ef
rwilhel <rwilhel@gmail.com>
2017-10-03 09:38:29 -0600

    Adds a dismiss button with removal functionality

--- src/App.js
+++ src/App.js
@@ -27,12 +27,20 @@ class App extends Component {
     this.state = {
       list: list,
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 
   render() {
     return (
       <div className="App">
-        { this.state.list.map(item => 
+        { this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -40,6 +48,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
