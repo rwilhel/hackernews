bbf1adb818ae533b9b3014681cd3ddff3146de42
rwilhel <rwilhel@gmail.com>
2017-10-03 10:09:34 -0600

    Adds search text to search input field

--- src/App.js
+++ src/App.js
@@ -54,38 +54,15 @@ class App extends Component {
         <Search
           value={searchTerm}
           onChange={this.onSearchChange}
-        />
+        >
+          Search
+        </Search>
         <Table
           list={list}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
       </div>
-        <form>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange}
-           />
-        </form>
-        { list.filter(isSearched(searchTerm)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
-        )}
     );
   }
 }
