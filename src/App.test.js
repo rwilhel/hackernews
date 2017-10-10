a1c5e9193e5b292cd7c6fd5ba772ff8d2b489961
rwilhel <rwilhel@gmail.com>
2017-10-10 13:21:14 -0600

    Writes and passes test for rendering Table component and Snapshot of Table component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, { Search, Button } from './App';
+import App, { Search, Button, Table } from './App';
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -48,4 +48,27 @@ describe('Button', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+});
+
+describe('Table', () => {
+
+    const props = {
+      list: [
+        {title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+        {title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+      ],
+    };
+
+    it('renders', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<Table { ...props }/>, div);
+    });
+
+    test('snapshots', () => {
+      const component = renderer.create(
+        <Table { ...props }/>
+      );
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
 });
